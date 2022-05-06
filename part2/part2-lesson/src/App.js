30f753710b6fb3abfa0c48c109b779d15436b553
Jordan <jordan.foster11@gmail.com>
2022-05-06 15:40:17 -0700

    part2-d lesson

--- part2/part2-lesson/src/App.js
+++ part2/part2-lesson/src/App.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 
 import Note from './components/Note';
+import Notification from './components/Notification';
+import Footer from './components/Footer';
 
 import noteService from './services/notes';
 
@@ -9,6 +10,7 @@ const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     noteService
@@ -44,7 +46,6 @@ const App = () => {
   }
 
   const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/notes/${id}`;
     const note = notes.find(n => n.id === id);
     const changedNote = { ...note, important: !note.important };
 
@@ -54,7 +55,14 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote));
       })
       .catch(error => {
-        alert(`the note '${note.content}' was already deleted from the server`);
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        );
+
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+
         setNotes(notes.filter(n => n.id !== id));
       });
   }
@@ -62,6 +70,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -80,6 +89,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   );
 }
